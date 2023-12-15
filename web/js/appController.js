9816b9aaeff88b40bb4f5f76782282712378ff06
ryan_oracle <0050211@gmail.com>
2023-12-14 19:40:25 -0800

    Oracle JET - Lesson 1 - Part 8: Oracle JET Modules, add Ryan1 page

--- web/js/appController.js
+++ web/js/appController.js
@@ -38,7 +38,8 @@ define(['knockout', 'ojs/ojcontext', 'ojs/ojmodule-element-utils', 'ojs/ojknocko
         { path: 'dashboard', detail: { label: 'Dashboard', iconClass: 'oj-ux-ico-bar-chart' } },
         { path: 'incidents', detail: { label: 'Incidents', iconClass: 'oj-ux-ico-fire' } },
         { path: 'customers', detail: { label: 'Customers', iconClass: 'oj-ux-ico-contact-group' } },
-        { path: 'about', detail: { label: 'About', iconClass: 'oj-ux-ico-information-s' } }
+        { path: 'about', detail: { label: 'About', iconClass: 'oj-ux-ico-information-s' } },
+        { path: 'ryan1', detail: { label: 'Ryan1', iconClass: 'oj-ux-ico-information-s' } }
       ];
 
       // Router setup
